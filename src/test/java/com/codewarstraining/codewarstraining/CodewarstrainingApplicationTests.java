9d3c9a43e290112ef3d6c5ec7b3344ca3f74642a
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2020-05-03 13:14:27 +0200

    initial commit'

---
+++ src/test/java/com/codewarstraining/codewarstraining/CodewarstrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewarstraining.codewarstraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewarstrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
