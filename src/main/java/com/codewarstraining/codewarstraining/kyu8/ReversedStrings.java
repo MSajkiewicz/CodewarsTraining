fb1f2184b2508a32888405100dd0483076cbbbe8
Marcin Sajkiewicz <marcinsajkiewicz91@gmail.com>
2020-07-08 18:38:17 +0200

    Reversed Strings

--- src/main/java/com/codewarstraining/codewarstraining/kyu8/ReversedStrings.java
+++ src/main/java/com/codewarstraining/codewarstraining/kyu8/ReversedStrings.java
@@ -1,4 +1,12 @@
 package com.codewarstraining.codewarstraining.kyu8;
 
 public class ReversedStrings {
+
+    public static String solution(String str) {
+        StringBuilder string = new StringBuilder();
+        string.append(str);
+        string = string.reverse();
+        return string.toString();
+    }
+
 }
--- src/test/java/com/codewarstraining/codewarstraining/kyu8/ReversedStringsTest.java
+++ src/test/java/com/codewarstraining/codewarstraining/kyu8/ReversedStringsTest.java
@@ -1,7 +1,18 @@
 package com.codewarstraining.codewarstraining.kyu8;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReversedStringsTest {
+
+    @Test
+    public void reverseStringTest(){
+        //Given, When, Then
+        assertEquals(ReversedStrings.solution("abc"), "cba");
+    }
+
+
+
 
 }
